f9a9a1891c18f4164574bffe2fc2166acb7b6e94
Blaisehala <73476838+Blaisehala@users.noreply.github.com>
2024-04-26 03:40:55 -0400

    adds the update view

---
+++ select_to_update.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'db.php';
+include 'display.php';
+
+echo "<h2> Display the information </h2>"; 
+
+display("SELECT * FROM customer;");
+?> 
+
+<br/>
+<form action="fill_to_update.php" method="post">
+<h2>Name to Select: </h2>
+Customer Name: <input type="text" name="customer_name"/><br>
+<input type="Submit" value= "Select"/><input type="Reset"/>
+</form>     
